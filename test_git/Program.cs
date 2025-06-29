210b69db3c7839d48903174e27946ccc2628c012
quang <quangqquu@gmail.com>
2025-06-29 08:36:11 +0700

    #

--- test_git/Program.cs
+++ test_git/Program.cs
@@ -1,12 +1,253 @@
-﻿namespace test_git
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+using System.Threading;
+
+
+
+
+
+namespace test_git
 {
+    
+   
+        
+    
     internal class Program
     {
+        static object consoleLock = new object();
+
+
+        private static List<work_1> ocu = new List<work_1>();
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            int a, b;
-            Console.WriteLine("nhap a,b");
+            int total, length;
+            char c = ' ' ;
+            
+            while (true)
+            {
+            start:
+                Console.Write("nhap cong viec Y/N: ");
+                try { c = Convert.ToChar(Console.ReadLine()); }
+                catch (Exception)
+                {
+
+                }
+
+                while (c != 'Y' && c != 'N')
+                {
+                    Console.Write("nhap sai dinh dang: ");
+                    goto start;
+                }
+                if (c == 'Y')
+                {
+                    goto Action;
+                }
+                else
+                {
+                    goto printf;
+                }
+            Action:
+                work_1 peo = new work_1();
+                Console.WriteLine("nhap cong viec: ");
+                peo.Name = Console.ReadLine();
+                Console.WriteLine("nhap thoi gian : ");
+                peo.Toltal_time = int.Parse(Console.ReadLine());
+                Console.WriteLine("nhap do dai : ");
+                peo.Length = int.Parse(Console.ReadLine());
+                ocu.Add(peo);
+               
+            }
+        printf:
+            if (ocu.Count > 0)
+            {
+                goto dooo;
+            }
+            else
+            {
+                goto End;
+            }
+
+        dooo:
+            int d = 0;
+            float[] A = new float[2];
+            
+                for (int j = 0 ; j <= ocu[0].Toltal_time; j++) 
+                {
+                    foreach (work_1 a in ocu)
+                    {
+                        a.Trang_thai_1 = a.Trang_thai( j);
+                    A = a.time(j);
+                    a.Remaning_time = A[0];
+                    a.Con_time = (int)A[1];
+                        Console.WriteLine(a);
+                    }
+                    Thread.Sleep(1000);
+                    clear(ocu.Count);
+                }
+        End:
+            Console.WriteLine("end...");
+
+
         }
+        private void Calculator_1(int a, int b)
+        {
+
+            Console.WriteLine("tong :" + (a + b));
+            Console.WriteLine("hiêu : " + (a - b));
+            Console.WriteLine("tich  :" + (a * b));
+            Console.WriteLine("thuong : " + (a / b));
+            Console.WriteLine("so du : " + (a % b));
+
+        }
+        static void tryparse_1(string a)
+        {
+            int b;
+            int.TryParse(a, result: out b);
+            Console.WriteLine(b);
+        }
+        static void convert_1(string a)
+        {
+            int b;
+            try { b = Convert.ToInt32(a); }
+            catch (FormatException)
+            {
+                Console.WriteLine("loi format");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("tran bo nho");
+            }
+
+        }
+        static void old_1(String c)
+        {
+            DateTime b;
+            while (!DateTime.TryParse(c, out b))
+            {
+                Console.WriteLine("nhap lai ");
+                c = Console.ReadLine();
+            }
+            Console.WriteLine(Convert.ToInt32((DateTime.Now - b).TotalDays / 365) + "  tuổi  " + Convert.ToInt32((DateTime.Now - b).TotalDays % 365) + "  ngay");
+            Console.WriteLine((DateTime.Now.Year - b.Year));
+        }
+        static void clock_1(int a)
+        {
+            int c = 0;
+            Console.WriteLine("start clock .....");
+            for (int i = a; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+                thanhtrangthai(c, a);
+                Thread.Sleep(2000);
+                clear_1(2);
+                c++;
+
+
+            }
+
+            //clear_1(2);
+            Console.WriteLine("end clock .....");
+
+        }
+        static void clear_1(int a)
+        {
+            int c;
+            if ((Console.CursorTop - a) > 0) { c = Console.CursorTop - 1; }
+            else { c = Console.CursorTop; }
+            Console.SetCursorPosition(0, c);
+            Console.WriteLine("  ", Console.WindowWidth);
+            Console.WriteLine("  ", Console.WindowWidth);
+            Console.SetCursorPosition(0, c);
+        }
+        static void thanhtrangthai(int a,int s)
+        { 
+            int b;
+            if(a!= 0) 
+            {  
+                b = 50 -(int)(50 * a / s); 
+            }
+            else { b = 50; }
+
+
+
+                for (int j = 0; j < 50-b; j++)
+                {
+                    Console.Write(" ");
+                }
+            for (int j = 0; j < b + 1; j++)
+            {
+                Console.Write(":");
+            }
+        }
+        static void snt(int a)
+        {
+            int b = Convert.ToInt32(Math.Sqrt(a));
+            bool c = false;
+            for (int i = 2; i <= b; i++)
+            {
+                if (a % i == 0)
+                {
+                    c = true;
+                    break;
+                }
+                else
+                {
+                    c = false;
+                }
+            }
+            if (c)
+            {
+                Console.WriteLine("ko phai snt");
+            }
+            else
+            {
+                Console.WriteLine(" snt");
+
+            }
+
+        }
+        static void check_string_1(string c)
+        {
+            int a = c.Length;
+            int letter =0, digital=0, whitespace= 0;
+            for (int i = 0; i < a; i++)
+            {
+                if (Char.IsLetter(c[i]))
+                {
+                    letter++;
+                }
+                else if (Char.IsDigit(c[i]))
+                {
+                    digital++;
+                }
+                else if (Char.IsWhiteSpace(c[i]))
+                {
+                    whitespace++;
+                }
+
+            }
+            Console.WriteLine(letter);
+            Console.WriteLine( whitespace);
+            Console.WriteLine( digital );
+        }
+        static void clear(int num_line)
+        {
+            int a;
+            if ((Console.CursorTop - num_line) > 0)
+            { a = Console.CursorTop - num_line; }
+            else { a = Console.CursorTop; }
+            Console.SetCursorPosition(0, a);
+            for (int j = 0; j < num_line; j++) { Console.Write(new string(' ', Console.WindowWidth)); }
+            Console.SetCursorPosition(0, a);
+
+        }
+
     }
 }
+
---
+++ test_git/people.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_git
+{
+    public class people
+    {
+        // ptroperty
+        private string _name;
+        private string _occupation;
+        private int _age;
+        //
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value)){
+                    throw new ArgumentException("nhap sai ten");
+                }
+                _name = value.Trim();
+            }
+        }
+        public string Occupation
+        {
+            get => _occupation;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value)){
+                    throw new ArgumentException("nhap sai ten");
+                }
+                _occupation = value.Trim();
+            }
+        }
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                if (value == null)
+                    throw new ArgumentException("nhap sai so");
+                _age = value;
+            }
+        }
+        // list
+        public override string ToString()
+        {
+            return $"Họ tên: {Name}, Tuổi: {Age}, nghề nghiệp:  {Occupation}";
+        }
+
+    }
+    public class work_1
+    {
+        private int _length;
+        private string _name;
+        private float _remainingtime;
+        private int _toltal_time;
+        private int con_time;
+        private string _trang_thai;
+        //get_set
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+        public string Trang_thai_1
+        {
+            get => _trang_thai;
+            set => _trang_thai = value;
+        }
+        public int Toltal_time
+        {
+            get => _toltal_time;
+            set => _toltal_time = value;
+        }
+        public int Length
+        {
+            get => _length;
+            set => _length = value;
+        }
+        public float Remaning_time
+        {
+            get => _remainingtime;
+            set => _remainingtime = value;
+
+        }
+        public int Con_time
+        {
+            get => con_time;
+            set => con_time = value;
+        }
+
+
+        public string Trang_thai(int now_index)
+        {
+            int current = (int)Length * (now_index) / Toltal_time;
+            string a;
+            if (now_index < Toltal_time) {  a = new string('-', current) + new string(':', Length - current); }
+            else { a = "hoan thanh"; }
+
+                
+            return a;
+        }
+        public float[] time(int now)
+        {
+            float[] a = new float[2];
+            a[0] = ((float)now / (float)Toltal_time)*100 ;
+            a[1] = Toltal_time - now;
+            return a;
+        }
+        
+           
+        public override string ToString()
+        {
+            return $"{_name} : {_remainingtime:F2} %   :  {_trang_thai}   {con_time}"  
+            ;
+        }
+    }
+    
+
+
+}
